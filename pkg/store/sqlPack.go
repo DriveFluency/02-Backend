ffad8e1799fb229de06223fe90f030ab904c92fd
estefape <93286782+estefape@users.noreply.github.com>
2024-06-25 00:48:48 -0300

    Fix nombre de tabla packs (#8)

--- pkg/store/sqlPack.go
+++ pkg/store/sqlPack.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+
 	"github.com/DriveFluency/02-Backend/internal/domain"
 )
 
@@ -21,56 +22,55 @@ func NewSqlPack(db *sql.DB) PackInterface {
 func (p *sqlPack) Read(id int) (domain.Pack, error) {
 
 	Pack := domain.Pack{}
-	query:="SELECT * FROM Packs WHERE id=? ;"
-	row := p.db.QueryRow(query,id)
-	err:= row.Scan(&Pack.Id,&Pack.Name,&Pack.Description,&Pack.NumberClasses,&Pack.DurationClasses,&Pack.Cost)
-	if err != nil{
-		return domain.Pack{},err
+	query := "SELECT * FROM packs WHERE id=? ;"
+	row := p.db.QueryRow(query, id)
+	err := row.Scan(&Pack.Id, &Pack.Name, &Pack.Description, &Pack.NumberClasses, &Pack.DurationClasses, &Pack.Cost)
+	if err != nil {
+		return domain.Pack{}, err
 	}
-	return  Pack,nil
+	return Pack, nil
 }
 
-func (p *sqlPack) Create(Pack domain.Pack)  error {
+func (p *sqlPack) Create(Pack domain.Pack) error {
 
 	var id int
-	queryExists := "SELECT id FROM Packs WHERE name = ?"
-	row := p.db.QueryRow(queryExists,Pack.Name)
-	err:= row.Scan(&id)
-	if err == nil{
-		    fmt.Println(id)
-			return errors.New("Pack existente")
+	queryExists := "SELECT id FROM packs WHERE name = ?"
+	row := p.db.QueryRow(queryExists, Pack.Name)
+	err := row.Scan(&id)
+	if err == nil {
+		fmt.Println(id)
+		return errors.New("Pack existente")
 	}
-	query:="INSERT INTO Packs(name,description,number_classes,duration_classes,cost) VALUES (?,?,?,?,?);"
-	stmt,err := p.db.Prepare(query)
-	if err != nil{
+	query := "INSERT INTO packs(name,description,number_classes,duration_classes,cost) VALUES (?,?,?,?,?);"
+	stmt, err := p.db.Prepare(query)
+	if err != nil {
 		return err
 	}
 
-	res,err := stmt.Exec(Pack.Name,Pack.Description,Pack.NumberClasses,Pack.DurationClasses,Pack.Cost)	
-	if err != nil{
+	res, err := stmt.Exec(Pack.Name, Pack.Description, Pack.NumberClasses, Pack.DurationClasses, Pack.Cost)
+	if err != nil {
 		return err
 	}
-	_,err = res.RowsAffected()
-	if err != nil{
+	_, err = res.RowsAffected()
+	if err != nil {
 		return err
 	}
 	return nil
 }
 
+func (p *sqlPack) Update(Pack domain.Pack) error {
 
-func (p *sqlPack) Update(Pack domain.Pack)  error {
-
-	query:="UPDATE Packs SET name=?, description=?, number_classes=?, duration_classes=?, cost=? WHERE id=? ;"
-	stmt,err := p.db.Prepare(query)
-	if err != nil{
+	query := "UPDATE packs SET name=?, description=?, number_classes=?, duration_classes=?, cost=? WHERE id=? ;"
+	stmt, err := p.db.Prepare(query)
+	if err != nil {
 		return err
 	}
-	res,err := stmt.Exec(Pack.Name,Pack.Description,Pack.NumberClasses,Pack.DurationClasses,Pack.Cost,Pack.Id)	
-	if err != nil{
+	res, err := stmt.Exec(Pack.Name, Pack.Description, Pack.NumberClasses, Pack.DurationClasses, Pack.Cost, Pack.Id)
+	if err != nil {
 		return err
 	}
-	_,err = res.RowsAffected()
-	if err != nil{
+	_, err = res.RowsAffected()
+	if err != nil {
 		return err
 	}
 
@@ -78,17 +78,17 @@ func (p *sqlPack) Update(Pack domain.Pack)  error {
 }
 func (p *sqlPack) Delete(id int) error {
 
-	query:="DELETE FROM Packs WHERE id=? "
-	stmt,err := p.db.Prepare(query)
-	if err != nil{
+	query := "DELETE FROM packs WHERE id=? "
+	stmt, err := p.db.Prepare(query)
+	if err != nil {
 		return err
 	}
-	res,err := stmt.Exec(id)	
-	if err != nil{
+	res, err := stmt.Exec(id)
+	if err != nil {
 		return err
 	}
-	_,err = res.RowsAffected()
-	if err != nil{
+	_, err = res.RowsAffected()
+	if err != nil {
 		return err
 	}
 
@@ -96,26 +96,25 @@ func (p *sqlPack) Delete(id int) error {
 
 }
 
-
 func (p *sqlPack) GetAll() ([]domain.Pack, error) {
-	
+
 	Packs := []domain.Pack{}
-	query:="SELECT * FROM Packs  ;"
-	rows,err := p.db.Query(query)
-	if err != nil{
+	query := "SELECT * FROM packs  ;"
+	rows, err := p.db.Query(query)
+	if err != nil {
 		log.Fatal(err)
 	}
 	err = rows.Err()
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
-	for rows.Next(){
+	for rows.Next() {
 		var Pack domain.Pack
-		err:= rows.Scan(&Pack.Id,&Pack.Name,&Pack.Description,&Pack.NumberClasses,&Pack.DurationClasses,&Pack.Cost)
-		if err != nil{
+		err := rows.Scan(&Pack.Id, &Pack.Name, &Pack.Description, &Pack.NumberClasses, &Pack.DurationClasses, &Pack.Cost)
+		if err != nil {
 			log.Fatal(err)
 		}
-		Packs = append(Packs,Pack )
+		Packs = append(Packs, Pack)
 	}
-	return Packs,nil
+	return Packs, nil
 }
